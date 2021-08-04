9add4ea0a03430e6f7d4c95d2defe16980a7fb81
ioutime <xiaoqiusg@gmail.com>
2021-08-04 23:51:07 +0800

    支持多账号部署； 实现了cookie签到显示昵称

--- action.py
+++ action.py
@@ -10,6 +10,8 @@ import argparse
 import execjs
 import re
 from http import cookiejar
+from urllib.parse import unquote
+import json
 
 # 创建一个session,作用会自动保存cookie
 Session = requests.session()
@@ -22,16 +24,56 @@ def main(infos):
     phone = infos["phone"]
     password = infos["password"]
     cookie = infos["cookie"]
+    p00001 = infos["p00001"]
     # Run tasks
-    # login(infos,phone,encry(password),cookie)
     if not cookie:
-        print("需要在secrets中添加cookie")
-        push_info(infos,"需要在secrets中添加cookie")
-        return
+        if not p00001:
+            print("需要在secrets中添加cookie")
+            push_info(infos,"需要在secrets中添加cookie")
+            return
+        else:
+            p00001s = p00001.split(',')
+            more_accounts(infos,p00001s)
+            return
+    elif cookie and p00001:
+        p00001s = p00001.split(',')
+        more_accounts(infos,p00001s)
+        return                  
     elif not password or not phone:
         transform(infos,cookie)
     else:
         login(infos,phone,encry(password),cookie)
+
+def more_accounts(infos,p00001):
+    '''
+    多账号签到
+    '''
+    ans = ''
+    for i in p00001:
+        ans = ans + "第%s账号：" % (p00001.index(i) + 1)
+        print ("第%s账号：" % (p00001.index(i) + 1))
+        if len(i) >= 20:
+            dct = {}
+            dct['P00001']=i
+            #签到
+            msg  = member_sign(dct)
+            if msg != "输入的cookie有问题(P00001)，请重新获取" and msg!='出错了，无法获取platform' and msg!='未签到成功,可能是程序问题,也可能你不是爱奇艺会员':
+                ans = ans + '   签到成功\n'
+            else:
+                ans = ans + '   FALSE\n'
+            print(msg)
+        else:
+            msg = ' p00001不完整'
+            ans = ans + '   p00001不完整\n'
+            print(msg)
+        print('='*40)
+    # print(ans)
+    #推送
+    if len(ans) <= 300:
+        push_info(infos,ans)
+    else:
+        push_info(infos,ans[0:200]+'......')
+    
 
 def push_info(infos,msg):
     '''
@@ -43,7 +85,11 @@ def push_info(infos,msg):
     else: 
         try:
             url = "http://www.pushplus.plus/send?token="+token+"&title=爱奇艺打卡&content="+msg+"&template=html"
-            requests.get(url=url)
+            html = requests.get(url=url)
+            if html.status_code == 200:
+                pass
+            else:
+                print('推送失败')
         except Exception as e:
             print('推送失败')
             print(e)
@@ -58,13 +104,15 @@ def get_args():
     parser.add_argument("-w", dest="password",help="The plaint text value of the password.")
     parser.add_argument("-t", dest="token", help="pushplus的token值")
     parser.add_argument("-c", dest="cookie", help="cookie值")
+    parser.add_argument("-s", dest="p00001", help="P00001值")
     args = parser.parse_args()
 
     return {
         "phone": args.phone,
         "password": args.password,
         "token" : args.token,
-        "cookie" : args.cookie
+        "cookie" : args.cookie,
+        "p00001" : args.p00001
     }
  
 def encry(password):
@@ -105,9 +153,8 @@ def login(infos,phone,password,cookie):
             transform(infos,cookie)
             return
         if(msg == '安全校验不通过'):
-            msg = msg + "\n可能是触发了滑块验证，或者设备锁未关"
-            print(msg)
-            print("安全校验不通过,使用cookie签到")
+            msg = "触发了安全验证，或者设备锁未关"
+            print(msg+"\n安全校验不通过,使用cookie签到")
             transform(infos,cookie)
             return
         data = html.get('data')
@@ -115,8 +162,7 @@ def login(infos,phone,password,cookie):
             nickname = data.get('nickname')
         except:
             msg = msg + "登录失败－nickname"
-            print(msg)
-            print("使用cookie签到")
+            print(msg+"\n使用cookie签到")
             transform(infos,cookie)
             return  
         print('='*40)
@@ -127,7 +173,6 @@ def login(infos,phone,password,cookie):
         msg0  = member_sign(cookies_dict)
         #获取用户信息
         msg1 = get_info(cookies_dict)
-        Session.close()
         #输出信息
         msg = msg0 + msg1
         print(msg)
@@ -172,7 +217,7 @@ def member_sign(cookies_dict):
         msg = f"{sign_msg}\n{growth}\n连续签到：{continueSignDaysSum}天\n签到周期：{rouund_day}天/{rewardDay}天\n"
     except Exception as e:
         print(e)
-        msg = "出错了,未签到成功,可能是程序问题,也可能你不是爱奇艺会员"
+        msg = "未签到成功,可能是程序问题,也可能你不是爱奇艺会员"
     return msg
 
 
@@ -197,9 +242,11 @@ def get_info(cookies_dict):
             deadline = res_data["deadline"]
             msg = f"VIP等级：{level}\n升级需成长值：{distance}\nVIP到期时间:{deadline}"
         except:
-            msg = "获取具体信息失败"
+            print("获取具体信息失败")
+            msg = ""
     else:
-        msg = "获取个人信息失败"
+        print("获取个人信息失败")
+        msg = ""
     return msg
 
 def logout(nickname,cookies):
@@ -226,7 +273,7 @@ def logout(nickname,cookies):
 
 def transform(infos,cookie):
     '''
-    转换获取的COOKIE
+    转换完整的COOKIE，并执行签到等功能
     '''
     try:
         cookies = cookie.replace(' ','')
@@ -248,16 +295,37 @@ def transform(infos,cookie):
         print(msg0)
         push_info(infos,msg0)
         return
+    #获取nickname
+    nickname = ''
+    try:
+        text = dct.get('P00002')
+        text = unquote(text, 'utf-8').encode('utf-8').decode('unicode_escape')
+        text = json.loads(text)
+        nickname = text.get('nickname') + ': '
+        #判断是否是会员
+        text2 = dct.get('QC179')
+        text2 = unquote(text2, 'utf-8').encode('utf-8').decode('unicode_escape')
+        text2 = json.loads(text2)
+        vipTypes = text2.get('vipTypes')
+        if vipTypes != '1':
+            nickname = nickname +'不是会员'
+            print(nickname)
+            push_info(infos,nickname)
+            return
+    except Exception as e:
+        print(e)
+        nickname = ''
     #签到
-    msg0  = member_sign(dct)
-    # 获取用户信息
-    # msg1 = get_info(dct)
-    # 输出信息
-    # msg0 = msg0 + msg1
-    print(msg0)
-    push_info(infos,msg0)
+    msg0  = nickname + member_sign(dct)
+    #用户信息
+    msg = msg0 + get_info(dct)
+    print(msg)
+    push_info(infos,msg)
     return
 
 
 if __name__=="__main__":
-    main(get_args())
+    try:
+        main(get_args())
+    finally:
+        Session.close()
